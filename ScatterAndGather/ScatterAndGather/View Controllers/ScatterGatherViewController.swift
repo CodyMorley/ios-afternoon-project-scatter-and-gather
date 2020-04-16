74c98b90fea2939187663b00487f353197db83ab
Cody Morley <codymorleyinc@gmail.com>
2020-04-15 17:11:42 -0700

    Finished instructions. doesn't look right. investigating.

--- ScatterAndGather/ScatterAndGather/View Controllers/ScatterGatherViewController.swift
+++ ScatterAndGather/ScatterAndGather/View Controllers/ScatterGatherViewController.swift
@@ -42,16 +42,66 @@ class ScatterGatherViewController: UIViewController {
     ///Below are the animations for the ImageView and the labels.
     
     func scatter() {
-        
+        ///1.Create an animation block to hold your key frames, use addKeyFrames
+        let animationBlock = {
+            ///a.Make the image go opaque
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.375) {
+                self.lambdaLogo.alpha = 0.0
+            }
+            
+            ///b.Make the letters "fall", this should start just after the image begins to go opaque but before it disappears.
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 1) {
+                self.lTextLabel.center = CGPoint(x: CGFloat.random(in: 0...self.view.bounds.size.width), y: CGFloat.random(in: 0...self.view.bounds.size.height))
+                self.aTextLabel1.center = CGPoint(x: CGFloat.random(in: 0...self.view.bounds.size.width), y: CGFloat.random(in: 0...self.view.bounds.size.height))
+                self.mTextLabel.center = CGPoint(x: CGFloat.random(in: 0...self.view.bounds.size.width), y: CGFloat.random(in: 0...self.view.bounds.size.height))
+                self.bTextLabel.center = CGPoint(x: CGFloat.random(in: 0...self.view.bounds.size.width), y: CGFloat.random(in: 0...self.view.bounds.size.height))
+                self.dTextLabel.center = CGPoint(x: CGFloat.random(in: 0...self.view.bounds.size.width), y: CGFloat.random(in: 0...self.view.bounds.size.height))
+                self.aTextLabel2.center = CGPoint(x: CGFloat.random(in: 0...self.view.bounds.size.width), y: CGFloat.random(in: 0...self.view.bounds.size.height))
+            }
+            
+            ///c. turn the letter frames slightly on their way toward their destination
+            UIView.addKeyframe(withRelativeStartTime: 0.375, relativeDuration: 1) {
+                self.lTextLabel.transform = CGAffineTransform(rotationAngle: .pi / CGFloat.random(in: 45...315))
+                self.aTextLabel1.transform = CGAffineTransform(rotationAngle: .pi / CGFloat.random(in: 45...315))
+                self.mTextLabel.transform = CGAffineTransform(rotationAngle: .pi / CGFloat.random(in: 45...315))
+                self.bTextLabel.transform = CGAffineTransform(rotationAngle: .pi / CGFloat.random(in: 45...315))
+                self.dTextLabel.transform = CGAffineTransform(rotationAngle: .pi / CGFloat.random(in: 45...315))
+                self.aTextLabel2.transform = CGAffineTransform(rotationAngle: .pi / CGFloat.random(in: 45...315))
+            }
+            
+            ///d. Change the letter and background colors to random colors
+            UIView.addKeyframe(withRelativeStartTime: 0.375, relativeDuration: 0.625) {
+                self.lTextLabel.backgroundColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 0.75)
+                self.aTextLabel1.backgroundColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 0.75)
+                self.mTextLabel.backgroundColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 0.75)
+                self.bTextLabel.backgroundColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 0.75)
+                self.dTextLabel.backgroundColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 0.75)
+                self.aTextLabel2.backgroundColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 0.75)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.375, relativeDuration: 0.625) {
+                self.lTextLabel.textColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 1)
+                self.aTextLabel1.textColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 1)
+                self.mTextLabel.textColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 1)
+                self.bTextLabel.textColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 1)
+                self.dTextLabel.textColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 1)
+                self.aTextLabel2.textColor = UIColor(red: CGFloat.random(in: 0...255), green: CGFloat.random(in: 0...255), blue: CGFloat.random(in: 0...255), alpha: 1)
+            }
+        }
+        ///2. Use the key frames you made to make an anmiateKeyFramesAnimation
+        UIView.animateKeyframes(withDuration: 2.0, delay: 0, options: [], animations: animationBlock, completion: nil)
     }
     
     func gather() {
-        self.lTextLabel.transform = .identity
-        self.aTextLabel1.transform = .identity
-        self.mTextLabel.transform = .identity
-        self.bTextLabel.transform = .identity
-        self.dTextLabel.transform = .identity
-        self.aTextLabel2.transform = .identity
-        self.lambdaLogo.transform = .identity
+        
+        UIView.animate(withDuration: 1.5) {
+            self.lTextLabel.transform = .identity
+            self.aTextLabel1.transform = .identity
+            self.mTextLabel.transform = .identity
+            self.bTextLabel.transform = .identity
+            self.dTextLabel.transform = .identity
+            self.aTextLabel2.transform = .identity
+            self.lambdaLogo.transform = .identity
+        }
     }
 }
